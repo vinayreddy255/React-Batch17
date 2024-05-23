927a23f9e212b1f881b0562eab3f5e2a9a8777d8
Nidumusali Vinay <nvinay@assurecare.com>
2024-05-23 09:02:08 +0530

    js docs

---
+++ JavaScript/HelloWorld.js
@@ -0,0 +1,19 @@
+var city = "Bangalore";
+console.log(typeof city); // string
+console.log(city); // Bangalore;
+
+var phone = 123456;
+console.log(phone); // 123456
+console.log(typeof phone);
+
+var flag = true;
+console.log(flag);
+console.log(typeof flag);
+
+var none = null;
+console.log(none);
+console.log(typeof none);
+
+var empData;
+console.log(empData);
+console.log(typeof empData);
